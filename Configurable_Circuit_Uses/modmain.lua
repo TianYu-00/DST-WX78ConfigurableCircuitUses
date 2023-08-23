cfb4d884a9a9e7ed0fa353c15b8710216236ffef
Tian <66271788+TianYu-00@users.noreply.github.com>
2023-08-23 22:38:58 +0100

    Add files via upload
    Initial Update v1.0

---
+++ Configurable_Circuit_Uses/modmain.lua
@@ -0,0 +1,9 @@
+local require = GLOBAL.require
+local tuning = GLOBAL.TUNING
+
+require "constants"
+
+
+local usesValue = GetModConfigData("CustomUses_Value")
+tuning.WX78_MODULE_USES = usesValue
+        
